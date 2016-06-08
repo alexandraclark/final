1c76ed4e38df2b8097218d9e1b93cd07630e7ea4
alexandraclark <alexandraclark@uchicago.edu>
2016-06-08 03:18:53 -0500

    fixing route bug

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   post '/sessions' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  map.notify 'notifications', :controller => 'notifications', :action => 'notify'
   post 'notifications/notify' => 'notifications#notify'
   post 'twilio/voice' => 'twilio#voice'
 
