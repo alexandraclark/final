1d5ba3d61cdedfead96f887d49534134e3407cf8
alexandraclark <alexandraclark@uchicago.edu>
2016-06-08 04:19:40 -0500

    app respond to message

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,6 +8,27 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def sms
+    from = params[:From]
+    from = from.tr!('+','')
+
+    message = params[:Body]
+    message = message.split('#', 2)
+
+    @guest = Guest.find_by(:phone => from)
+    @event = Event.find(message[1])
+
+    if message[0] == "YES" and @guest and @event
+      invite = Invitation.where(:event_id => @event.id, :guest_id => @guest.id)
+      invite.RSVP = true
+      invite.attending = true
+      invite.save
+    elsif message[0] == "NO" and @guest and @event
+      invite = Invitation.where(:event_id => @event.id, :guest_id => @guest.id)
+      invite.RSVP = true
+      invite.attending = false
+      invite.save
+    end
+
   	response = Twilio::TwiML::Response.new do |r|
   	  r.Message 'Thanks for RSVPing!'
   	end
