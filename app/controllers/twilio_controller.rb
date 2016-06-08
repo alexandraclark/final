4c45708a5ce3c8a12c9621d6842dedf57756bf57
alexandraclark <alexandraclark@uchicago.edu>
2016-06-08 04:31:12 -0500

    troubleshooting

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -14,20 +14,23 @@ class TwilioController < ApplicationController
     message = params[:Body]
     message = message.split('#', 2)
 
+    puts message[0]
+    puts message[1]
+
     @guest = Guest.find_by(:phone => from)
     @event = Event.find(message[1])
 
     if message[0] == "YES" and @guest and @event
-      invite = Invitation.where(:event_id => @event.id, :guest_id => @guest.id)
-      invite.RSVP = true
-      invite.attending = true
-      invite.save
+      @invite = Invitation.where(:event_id => @event.id, :guest_id => @guest.id)
+      @invite.RSVP = true
+      @invite.attending = true
+      @invite.save
     elsif message[0] == "NO" and @guest and @event
-      invite = Invitation.find_by(:event_id => @event.id, :guest_id => @guest.id)
-      puts invite
-      invite.RSVP = true
-      invite.attending = false
-      invite.save
+      @invite = Invitation.find_by(:event_id => @event.id, :guest_id => @guest.id)
+      # puts invite
+      @invite.RSVP = true
+      @invite.attending = false
+      @invite.save
     end
 
   	response = Twilio::TwiML::Response.new do |r|
