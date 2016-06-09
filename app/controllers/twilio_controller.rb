271fc830424b1fcfd2ee354408a28ac731d7261c
alexandraclark <alexandraclark@uchicago.edu>
2016-06-08 19:31:12 -0500

    bug

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -19,7 +19,7 @@ class TwilioController < ApplicationController
     puts message[1]
     puts message[2]
 
-    @guest = Guest.find_by(id: => message[2].to_i)
+    @guest = Guest.find_by(:id => message[2].to_i)
     @event = Event.find_by(:id => message[1].to_i)
 
     if message[0] == "YES" and @guest and @event
